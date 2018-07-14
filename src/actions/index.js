91adb7134976737bedad63f46d42a01dd25e2baa
Sergio <sergiogarher@gmail.com>
2018-07-14 20:25:43 +0200

    Correct typo + api url in action + bind fetchWeather action and SearchBar

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const API_KEY = 'e729a074f9321d16bec10e41d35b1ad6';
-export const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fecthWeather(city) {
+export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},es`;
     const request = axios.get(url);
 
