d81806249b9f858a0d040a83eb824004f86fc24c
Sergio <sergiogarher@gmail.com>
2018-07-14 20:08:20 +0200

    Install Axios for ajax calls + create fetchWeather action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = 'e729a074f9321d16bec10e41d35b1ad6';
+export const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fecthWeather(city) {
+    const url = `${ROOT_URL}&q=${city},es`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
